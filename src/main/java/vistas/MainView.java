8d3693f4bf9034f7fff2aefd19dfd6952b28b42e
agusvidal <agustinacandela2300@gmail.com>
2023-09-15 13:57:43 -0300

    cambio en recetas ui

--- src/main/java/vistas/MainView.java
+++ src/main/java/vistas/MainView.java
@@ -9,57 +9,83 @@ import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
+import java.util.Map;
 
-public class MainView extends JFrame implements PropertyChangeListener
-{
+public class MainView extends JFrame implements PropertyChangeListener {
     private JComboBox<String> comboBox;
     private JPanel panel;
     private JLabel labelTitulo;
     private JButton btnRecommend;
+    private JTextArea lblRecommendations;
 
 
-    public MainView(ObservableChefExpress chefExpress)
-    {
+    public MainView(ObservableChefExpress chefExpress) {
         createViewComponents();
         MainController controller = new MainController(this, chefExpress);
         chefExpress.attach(this);
     }
 
-    private void createViewComponents()
-    {
+    private void createViewComponents() {
         setTitle("Chef Express");
 
-        panel = new JPanel(new BorderLayout());
-        labelTitulo = new JLabel("Chef Express");
-        panel.add(labelTitulo, BorderLayout.NORTH);
+        panel = new JPanel(new FlowLayout());
+        labelTitulo = new JLabel("¿Qué tipo de receta buscás?");
+        panel.add(labelTitulo);
 
-        String[] opciones = {" ", "Saludables"};
+        String[] opciones = {"Saludables"};
         comboBox = new JComboBox<>(opciones);
-        setLayout(new FlowLayout());
         panel.add(comboBox);
-        add(panel);
+
+        panel.add(Box.createVerticalStrut(10));
 
         this.btnRecommend = new JButton();
-        this.btnRecommend.setText( "Recommend" );
-        this.btnRecommend.setBounds( 10, 200, 100, 27 );
-        this.panel.add( this.btnRecommend);
+        this.btnRecommend.setText("Ver recomendaciones");
+        this.btnRecommend.setBounds(10, 230, 100, 27);
+        this.panel.add(this.btnRecommend);
 
-        setSize(300, 100);
+        addLabelRecommendations();
+
+        add(panel);
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public JButton getBtnRecommend()
-    {
+    private void addLabelRecommendations() {
+        this.lblRecommendations = new JTextArea();
+        this.lblRecommendations.setEditable(false);
+        this.lblRecommendations.setPreferredSize(new Dimension(300, 200));
+        panel.add(new JScrollPane(this.lblRecommendations));
+    }
+
+    public JButton getBtnRecommend() {
         return this.btnRecommend;
     }
 
     @Override
-     public void propertyChange(PropertyChangeEvent evt)
-     {
-         java.util.List<Recipe> recommendRecipes = (List<Recipe>) evt.getNewValue();
-         System.out.println(recommendRecipes.get(0).getName());
-     }
+    public void propertyChange(PropertyChangeEvent evt) {
+        java.util.List<Recipe> recommendRecipes = (List<Recipe>) evt.getNewValue();
+        showRecipes(recommendRecipes);
+    }
 
-     public void start()
-     { setVisible(true); }
- }
+    private void showRecipes(List<Recipe> recipes) {
+        StringBuilder recommendationsText = new StringBuilder();
+
+        for (Recipe recipe : recipes) {
+            recommendationsText.append("Receta: ").append(recipe.getName()).append("\n");
+
+            recommendationsText.append("Ingredientes:\n");
+            for (Map.Entry<String, Float> entry : recipe.getIngredients().entrySet()) {
+                String ingredientName = entry.getKey();
+                Float ingredientAmount = entry.getValue();
+                recommendationsText.append("- ").append(ingredientName).append(": ").append(ingredientAmount).append("\n");
+            }
+
+            recommendationsText.append("\n");
+        }
+        this.lblRecommendations.setText(recommendationsText.toString());
+    }
+
+    public void start() {
+        setVisible(true);
+    }
+}
