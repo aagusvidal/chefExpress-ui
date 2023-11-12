f8d01da2c6efd2751e9e7144d6558d57bb50b3be
agusvidal <agustinacandela2300@gmail.com>
2023-11-12 16:56:37 -0300

    Se agrega botón de cambiar recetas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,6 @@ import factories.ChefExpressBuilder;
 import factories.HistoricalRecipesCounterFactory;
 import vistas.MainView;
 
-
-import java.util.List;
-
 public class Main
 {
         public static void main(String[] args) throws Exception
